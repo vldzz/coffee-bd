0335c44cbf45331f75efbab5901c5b134c7ab14e
Alexenon <58091611+Alexenon@users.noreply.github.com>
2022-01-26 21:52:30 +0200

    Update Coffee_Time.sql

--- Coffee_Time.sql
+++ Coffee_Time.sql
@@ -1,3 +1,4 @@
+SET DATEFORMAT dmy
 USE master
 GO
 IF exists(
@@ -16,37 +17,31 @@ GO
 
 CREATE TABLE Subsidiaries(
 	id_subsidiary INT PRIMARY KEY,
-	subsidiary_name NVARCHAR(20) UNIQUE NOT NULL,
-	adress NVARCHAR(20) 
+	subsidiary_name NVARCHAR(50) UNIQUE NOT NULL,
+	adress NVARCHAR(50) 
 )
 
 CREATE TABLE Employers(
 	id_employer INT PRIMARY KEY,
-	first_name NVARCHAR(20) NOT NULL,
-	last_name NVARCHAR(20) NOT NULL,
-	sallary FLOAT CHECK (sallary >= 0) NOT NULL,
-	id_subsidiary INT FOREIGN KEY REFERENCES Subsidiaries(id_subsidiary)
+	first_name NVARCHAR(25) NOT NULL,
+	last_name NVARCHAR(25) NOT NULL,
+	sallary FLOAT NOT NULL,
+	id_subsidiary INT FOREIGN KEY REFERENCES Subsidiaries(id_subsidiary),
+	CHECK (sallary >= 0)
 )
 
 CREATE TABLE Providers(
 	id_provider INT PRIMARY KEY,
-	provider_name NVARCHAR(20) NOT NULL,
-	adress NVARCHAR(20)
+	provider_name NVARCHAR(25) NOT NULL,
+	adress NVARCHAR(25)
 )
 
 CREATE TABLE Products(
 	id_product INT PRIMARY KEY,
-	product_name NVARCHAR(20) UNIQUE NOT NULL,
+	product_name NVARCHAR(25) UNIQUE NOT NULL,
 	price FLOAT NOT NULL,
-	id_provider INT FOREIGN KEY REFERENCES Providers(id_provider) 
+	id_provider INT FOREIGN KEY REFERENCES Providers(id_provider),
 	CHECK (price >= 0 AND price <= 100)
-)
-
-CREATE TABLE Payments(
-	id_payment INT PRIMARY KEY,
-	quantity TINYINT DEFAULT 1 NOT NULL,
-	id_product INT FOREIGN KEY REFERENCES Products(id_product)
-	CHECK (quantity > 0)
 )
 
 CREATE TABLE Sales(
@@ -57,15 +52,69 @@ CREATE TABLE Sales(
 	id_employer INT FOREIGN KEY REFERENCES Employers(id_employer)
 )
 
-CREATE TABLE History(
-	id_history INT PRIMARY KEY,
-	date_payment DATETIME NOT NULL,
-
-
+CREATE TABLE Payments(
+	id_payment INT PRIMARY KEY,
+	id_product INT FOREIGN KEY REFERENCES Products(id_product),
+	quantity TINYINT DEFAULT 1 NOT NULL,
+	id_sale INT FOREIGN KEY REFERENCES Sales(id_sale),
+	CHECK (quantity > 0)
 )
 
 
 
-Select * FROM Sales
+INSERT INTO Subsidiaries VALUES 
+	(1, 'Centru', 'str.Stefan cel Mare 47'),
+	(2, 'Botanica', 'str.Cuza Voda 128'),
+	(3, 'Ciocana', 'str.Mircea cel Batran 26')
+
+INSERT INTO Employers VALUES
+	(1, 'Bolsoi', 'Valentina', 4800, 1),
+	(2, 'Covrig', 'Petru', 5200, 3),
+	(3, 'Frunza', 'Sanda', 5000, 2)
 
 
+INSERT INTO Providers(id_provider, provider_name, adress) VALUES 
+	(1, 'JDK', 'str 31 august 99'),
+	(2, 'CoffeSomeName', 'str. Dacia 43'),
+	(3, 'SiropMd', 'str. Stefan cel mare 65'),
+	(4, 'Linella', 'str. Titulescu 3')
+
+INSERT INTO Products(id_product, product_name, price, id_provider) VALUES 
+	(1, 'americano', 17, 2),
+	(2, 'cappucino', 22, 1),
+	(3, 'mochaccino', 25, 3),
+	(4, 'croasant', 15, 2),
+	(5, 'ceai negru', 20, 2)
+
+INSERT INTO Sales(id_sale, payment_type, sale_date, id_subsiadary, id_employer) VALUES
+	(1, 'CARD', GETDATE(), 2, 3),
+	(2, 'CASH', GETDATE(), 1, 1),
+	(3, 'CARD', GETDATE(), 2, 3)
+
+
+INSERT INTO Payments(id_payment, quantity, id_product, id_sale) VALUES
+	(1, 1, 1, 1),
+	(2, 2, 2, 2),
+	(3, 1, 5, 2),
+	(4, 2, 5, 3),
+	(5, 3, 2, 3)
+GO
+
+CREATE VIEW Show_Payments AS
+SELECT p.id_payment, pr.product_name, pr.price, p.quantity, (pr.price*p.quantity) as 'total_price', s.payment_type, s.sale_date, (e.first_name + ' ' + e.last_name) as cashier, s.id_sale
+FROM Payments p
+INNER JOIN Sales s ON p.id_sale = s.id_sale
+INNER JOIN Products pr ON p.id_product = pr.id_product
+INNER JOIN Employers e ON e.id_employer = s.id_employer
+GO
+
+
+
+-- Vizualiare cat si de cati bani a cumparat
+
+-- Tabel pentru adaousuri
+--		americano + lapte + sirop vanilie
+
+
+-- Celiku momentan poate lucra in 2 spoturi in aceiasi zi
+-- Tabel sales, check !
